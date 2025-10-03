48aee21dd4958757f04214a22b6d8bf93c71352f
Yonatan <yonatan.daniel54@gmail.com>
2025-10-03 11:59:51 +0300

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, DevOps!");
+console.log("Learing CI/CD with GitHub Actions");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.loge("Starting tests...");
+setTimeout(() => console.log("Waiting 3 seconds..."), 3000);
+console.log("Tests completed.");
